95398b1693ff7042b2ef6bc9058ac9422e1d62c8
mithunkumar123x <mithunkumar123x@gmail.com>
2024-04-03 18:23:51 +0530

    added functionality and button for changing order descending into ascending .

--- NumbersInUI/src/App.jsx
+++ NumbersInUI/src/App.jsx
@@ -1,32 +1,38 @@
-import React , {useCallback, useState }from 'react';
-import './App.css'
+import React, { useState, useCallback, useMemo } from 'react';
+import './App.css';
 import Button from './components/UI/Button/Button';
-import DemoOutput from './components/Demo/DemoOutput';
+import DemoList from './components/Demo/DemoList';
 
 function App() {
-  const [showParagraph, setShowParagraph] = useState(false);
-  const [allowToggle,setAllowToggle] = useState(false)
- 
- console.log('APP RUNNIG');
+  const [listTitle, setListTitle] = useState('My List');
+  const [order, setOrder] = useState('change to descending order');
+  const [change,setChange] = useState(false);
 
-  const toggleParagraphHandler = useCallback(() => {
-    if (allowToggle) {
-      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-      }
-    },[allowToggle])
+  const changeTitleHandler = useCallback(() => {
+    setListTitle('New Title');
+  }, []);
 
-    const allowToggleHandler = () => {
-      setAllowToggle(true);
+  const changeOrderHandler = () => {
+    if (!change) {
+      setOrder('change to ascending order');
+      setChange(true);
+    } else {
+      setOrder('change to descending order');
+      setChange(false);
     }
+  }
 
+ 
+const listItems = useMemo(() => [5,3,1,10,9] , [])
   return (
     <div className="app">
-     <h1>Hi There!</h1>
-     <DemoOutput show = {showParagraph} />
-     <Button onClick={allowToggleHandler}>allow Toggling</Button>
-     <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
+      <DemoList title={listTitle} items={listItems} change = {change} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
+      <Button onClick={changeOrderHandler}>
+        Change to Order
+      </Button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
